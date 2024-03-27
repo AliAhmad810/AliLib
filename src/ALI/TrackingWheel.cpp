f981a3dca28d38eab8fd8c59219030efbc095f60
AliAhmad810 <hiimaliahmad@gmail.com>
2024-03-27 16:38:41 -0700

    Fixed TrackingWheel.cpp

--- src/ALI/TrackingWheel.cpp
+++ src/ALI/TrackingWheel.cpp
@@ -48,32 +48,6 @@ float TrackingWheel::getDistanceTraveled() {
   }
 }
 
-
-float lemlib::TrackingWheel::getDistanceTraveled() {
-    if (this->encoder != nullptr) {
-        return (float(this->encoder->get_value()) * this->diameter * M_PI / 360) / this->gearRatio;
-    } else if (this->rotation != nullptr) {
-        return (float(this->rotation->get_position()) * this->diameter * M_PI / 36000) / this->gearRatio;
-    } else if (this->motors != nullptr) {
-        // get distance traveled by each motor
-        std::vector<pros::motor_gearset_e_t> gearsets = this->motors->get_gearing();
-        std::vector<double> positions = this->motors->get_positions();
-        std::vector<float> distances;
-        for (int i = 0; i < this->motors->size(); i++) {
-            float in;
-            switch (gearsets[i]) {
-                case pros::E_MOTOR_GEARSET_36: in = 100; break;
-                case pros::E_MOTOR_GEARSET_18: in = 200; break;
-                case pros::E_MOTOR_GEARSET_06: in = 600; break;
-                default: in = 200; break;
-            }
-            distances.push_back(positions[i] * (diameter * M_PI) * (rpm / in));
-        }
-        return lemlib::avg(distances);
-    } else {
-        return 0;
-    }
-}
 float TrackingWheel::getOffset() {
     return _distance;
 }
